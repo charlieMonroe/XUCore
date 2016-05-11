267808daac0c3f016dd7b28d7f034d97e32787e7
Charlie Monroe <charlie@charliemonroe.net>
2016-05-11 14:44:33 +0200

    MKGeometryAdditions.

---
+++ XUCore/additions/MKGeometryAdditions.swift
@@ -0,0 +1,31 @@
+//
+//  MKGeometryAdditions.swift
+//  XUCore
+//
+//  Created by Charlie Monroe on 5/11/16.
+//  Copyright © 2016 Charlie Monroe Software. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+public extension MKCoordinateRegion {
+	
+	/// Returns true is the region contains this location.
+	public func containsLocation(location: CLLocationCoordinate2D) -> Bool {
+		let minLatitude = self.center.latitude - self.span.latitudeDelta
+		let maxLatitude = self.center.latitude + self.span.latitudeDelta
+		
+		let minLongitude = self.center.longitude - self.span.longitudeDelta
+		let maxLongitude = self.center.longitude + self.span.longitudeDelta
+		
+		return (location.latitude >= minLatitude && location.latitude <= maxLatitude)
+			&& (location.longitude >= minLongitude && location.longitude <= maxLongitude)
+	}
+	
+	/// Convenience wrapper for CLLocation.
+	public func containsLocation(location: CLLocation) -> Bool {
+		return self.containsLocation(location.coordinate)
+	}
+	
+}
