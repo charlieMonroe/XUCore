0e607b200acbce8169eb1fb53da4c0fd970b2335
Charlie Monroe <charlie@charliemonroe.net>
2016-05-17 11:10:08 +0200

    Deprecating various NSBezierPath methods.

--- XUCore/additions/NSBezierPathAdditions.h
+++ XUCore/additions/NSBezierPathAdditions.h
@@ -26,33 +26,39 @@ typedef NS_ENUM(NSInteger, XUDirection) {
 
 @interface NSBezierPath (NSBezierPathAdditions)
 
-+(nonnull instancetype)bezierPathWithRoundRectAndTriangleInRect:(NSRect)inRect triangleCenteredToRect:(NSRect)centerRect windowFrame:(NSRect)windowFrame cornerRadius:(float)radius;
++(nonnull instancetype)bezierPathWithRoundRectAndTriangleInRect:(NSRect)inRect triangleCenteredToRect:(NSRect)centerRect windowFrame:(NSRect)windowFrame cornerRadius:(float)radius DEPRECATED_ATTRIBUTE;
 
 +(nonnull instancetype)bezierPathWithTriangleInRect:(NSRect)rect direction:(XUDirection)direction;
 
-+(nonnull instancetype)bezierPathWithCGPath:(nonnull CGPathRef)pathRef;
--(nonnull CGPathRef)cgPath __attribute__((cf_returns_retained));
++(nonnull instancetype)bezierPathWithCGPath:(nonnull CGPathRef)pathRef DEPRECATED_ATTRIBUTE;
+-(nonnull CGPathRef)cgPath __attribute__((cf_returns_retained)) DEPRECATED_ATTRIBUTE;
 
--(nonnull instancetype)pathWithStrokeWidth:(CGFloat)strokeWidth;
+-(nonnull instancetype)pathWithStrokeWidth:(CGFloat)strokeWidth DEPRECATED_ATTRIBUTE;
 
 -(void)fillWithInnerShadow:(nonnull NSShadow *)shadow;
--(void)drawBlurWithColor:(nonnull NSColor *)color radius:(CGFloat)radius;
+-(void)drawBlurWithColor:(nonnull NSColor *)color radius:(CGFloat)radius DEPRECATED_ATTRIBUTE;
 
 -(void)strokeInside;
 -(void)strokeInsideWithinRect:(NSRect)clipRect;
 
 @end
 
+DEPRECATED_ATTRIBUTE
 NS_INLINE NSRect FCCenteredRectInRect(NSRect frame, NSRect insideRect){
 	return NSMakeRect(NSMinX(frame) + (NSWidth(frame) - NSWidth(insideRect)) / 2.0, NSMinY(frame) + (NSHeight(frame) - NSHeight(insideRect)) / 2.0, NSWidth(insideRect), NSHeight(insideRect));
 }
+
+DEPRECATED_ATTRIBUTE
 NS_INLINE NSRect FCBottomHalfOfRect(NSRect rect){
 	return NSMakeRect(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height / 2.0);
 }
+
+DEPRECATED_ATTRIBUTE
 NS_INLINE NSRect FCTopHalfOfRect(NSRect rect){
 	return NSMakeRect(rect.origin.x, rect.origin.y + rect.size.height / 2.0, rect.size.width, rect.size.height / 2.0);
 }
 
+DEPRECATED_ATTRIBUTE
 NS_INLINE NSPoint FCRectCenter(NSRect rect){
 	return NSMakePoint(NSMinX(rect) + (NSWidth(rect) / 2.0), NSMinY(rect) + (NSHeight(rect) / 2.0));
 }
