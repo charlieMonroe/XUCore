5024f65379433f6ce58722961488080403ffe309
Charlie Monroe <charlie@charliemonroe.net>
2016-05-17 10:47:39 +0200

    Minor improvement to XUUIBridge.

--- XUCore/ui/XUUIBridge.swift
+++ XUCore/ui/XUUIBridge.swift
@@ -6,13 +6,13 @@
 //  Copyright © 2016 Charlie Monroe Software. All rights reserved.
 //
 
-import Foundation
-
 /// A type that defines a native view on the platform. On OS X, it's NSView,
 /// UIView on iOS.
 #if os(iOS)
+	import UIKit
 	public typealias __XUBridgedView = UIView
 #else
+	import Cocoa
 	public typealias __XUBridgedView = NSView
 #endif
 
