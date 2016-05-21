0387fbfb36a3db893d4d1b767ed9e9512b91c8c9
Charlie Monroe <charlie@charliemonroe.net>
2016-05-21 08:12:35 +0200

    XUThrowAbstractException now takes in a new argument - additionalInformation.

--- XUCore/core/XUAbstract.swift
+++ XUCore/core/XUAbstract.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 /** Throws an abstraction exception. */
-@noreturn public func XUThrowAbstractException(file: String = #file, line: Int = #line, method: String = #function) {
-	NSException(name: "XUAbstractExceptionName", reason: "[\(file.componentsSeparatedByString("/").last!):\(line) \(method)]", userInfo: nil).raise()
+@noreturn public func XUThrowAbstractException(additionalInformation: String = "", file: String = #file.componentsSeparatedByString("/").last!, line: Int = #line, method: String = #function) {
+	
+	let reason = "XUAbstractException: [\(file):\(line) \(method)] \(additionalInformation)"
+	NSException(name: "XUAbstractExceptionName", reason: reason, userInfo: nil).raise()
 	abort()
 }
 
