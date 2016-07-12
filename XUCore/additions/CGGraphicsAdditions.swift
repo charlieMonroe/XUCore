8bbfbc216996053a9b0fc079a91455770620f71e
Charlie Monroe <charlie@charliemonroe.net>
2016-07-12 18:12:33 +0200

    Improvements to localization.

--- XUCore/additions/CGGraphicsAdditions.swift
+++ XUCore/additions/CGGraphicsAdditions.swift
@@ -13,7 +13,13 @@ public extension CGRect {
 	public var center: CGPoint {
 		return CGPoint(x: self.midX, y: self.midY)
 	}
-	
+
+	/// Centers the rect so that self.center is the same as the returned value's.
+	public func centerRect(rect: CGRect) -> CGRect {
+		let origin = CGPoint(x: self.minX + (self.width - rect.width) / 2.0, y: self.minY + (self.height - rect.height) / 2.0)
+		return CGRect(origin: origin, size: rect.size)
+	}
+
 }
 
 
