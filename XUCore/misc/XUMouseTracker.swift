2ba1631503f9555e46eb6304c73c0de0bb5f7e68
Charlie Monroe <charlie@charliemonroe.net>
2016-08-12 11:19:09 +0200

    XURegex+BasicRegex - common expression were divided into separate structs.

--- XUCore/misc/XUMouseTracker.swift
+++ XUCore/misc/XUMouseTracker.swift
@@ -9,13 +9,13 @@
 import Foundation
 
 @objc public protocol XUMouseTrackingObserver: NSObjectProtocol {
-	
+
 	/// Called when the mouse clicked at point.
 	func mouseClickedAtPoint(point: CGPoint, atDisplay displayID: CGDirectDisplayID, withEventFlags flags: CGEventFlags)
-	
+
 	/// Called when the mouse moved to point.
 	func mouseMovedToPoint(point: CGPoint, atDisplay displayID: CGDirectDisplayID, withEventFlags flags: CGEventFlags)
-	
+
 }
 
 private func XUMouseMovementEventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutablePointer<Void>) -> Unmanaged<CGEvent>? {
@@ -27,30 +27,29 @@ private func XUMouseMovementEventCallback(proxy: CGEventTapProxy, type: CGEventT
 		XULog("No displays at { \(point.x), \(point.y) }")
 		return Unmanaged.passRetained(event)
 	}
-	
+
 	let flags = CGEventGetFlags(event)
 	let mouseTracker = unsafeBitCast(refcon, XUMouseTracker.self)
-	
+
 	if type == .MouseMoved {
 		mouseTracker._notifyObserversAboutMovementToPoint(point, atDisplay: displayID, withEventFlags: flags)
-	}else if type == .LeftMouseDown {
+	} else if type == .LeftMouseDown {
 		mouseTracker._notifyObserversAboutClickAtPoint(point, atDisplay: displayID, withEventFlags: flags)
 	}
-	
+
 	CGEventSetType(event, .MouseMoved)
 
 	return Unmanaged.passRetained(event)
 }
 
-
 /// This class allows mouse tracking.
 public class XUMouseTracker: NSObject {
-	
+
 	private let _lock = NSLock()
-	private var _observers: [XUMouseTrackingObserver] = [ ]
-	
+	private var _observers: [XUMouseTrackingObserver] = []
+
 	public static var sharedMouseTracker = XUMouseTracker()
-	
+
 	private func _notifyObserversAboutClickAtPoint(point: CGPoint, atDisplay displayID: CGDirectDisplayID, withEventFlags flags: CGEventFlags) {
 		_lock.performLockedBlock {
 			for observer in self._observers {
@@ -71,36 +70,36 @@ public class XUMouseTracker: NSObject {
 			XULog("NULL event port")
 			return
 		}
-		
+
 		guard let eventSrc = CFMachPortCreateRunLoopSource(nil, machPort, 0) else {
 			XULog("No event run loop src?")
 			return
 		}
-		
+
 		guard let runLoop = CFRunLoopGetCurrent() else {
 			XULog("No run loop?")
 			return
 		}
-		
-		CFRunLoopAddSource(runLoop,eventSrc,kCFRunLoopDefaultMode)
+
+		CFRunLoopAddSource(runLoop, eventSrc, kCFRunLoopDefaultMode)
 		CFRunLoopRun()
 	}
-	
+
 	public func addObserver(observer: XUMouseTrackingObserver) {
 		_lock.performLockedBlock {
 			XULog("adding an observer \(observer)")
 			self._observers.append(observer)
 		}
 	}
-	
+
 	public override init() {
 		super.init()
-		
+
 		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) { () -> Void in
 			self._trackingThread()
 		}
 	}
-	
+
 	public func removeObserver(observer: XUMouseTrackingObserver) {
 		_lock.performLockedBlock {
 			if let index = self._observers.indexOf({ $0 === observer }) {
@@ -108,7 +107,6 @@ public class XUMouseTracker: NSObject {
 			}
 		}
 	}
-	
-}
 
+}
 
