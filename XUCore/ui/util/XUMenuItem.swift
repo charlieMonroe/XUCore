c4ea7e3125980a62d4441b986cc734aa79fbda53
Charlie Monroe <charlie@charliemonroe.net>
2016-09-08 06:48:32 +0200

    XUMenuItem - a new NSMenuItem subclass that allows you to handle the action with a closure.

---
+++ XUCore/ui/util/XUMenuItem.swift
@@ -0,0 +1,34 @@
+//
+//  XUMenuItem.swift
+//  XUCore
+//
+//  Created by Charlie Monroe on 9/7/16.
+//  Copyright © 2016 Charlie Monroe Software. All rights reserved.
+//
+
+import Cocoa
+
+/// This is a specialized menu item subclass that allows using closures for handling
+/// menu item actions.
+public final class XUMenuItem: NSMenuItem {
+	
+	/// Action handler.
+	public let actionHandler: ((XUMenuItem) -> Void)?
+	
+	@objc private func _actionHandler(sender: XUMenuItem) {
+		self.actionHandler?(sender)
+	}
+	
+	/// Designated initializer.
+	public init(title: String, andActionHandler actionHandler: ((XUMenuItem) -> Void)? = nil) {
+		self.actionHandler = actionHandler
+		super.init(title: title, action: #selector(_actionHandler(_:)), keyEquivalent: "")
+		self.target = self
+	}
+	
+	public required init?(coder aDecoder: NSCoder) {
+		self.actionHandler = nil
+		super.init(coder: aDecoder)
+	}
+	
+}
