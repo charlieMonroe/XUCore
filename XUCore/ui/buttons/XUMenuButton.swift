4c989b5fae8bde33e04f3e7f70a51c3bcdebd456
Charlie Monroe <charlie@charliemonroe.net>
2017-04-11 10:16:28 +0200

    Enhancing XUMenuButton: 	- specify menu alignment, font and offset.

--- XUCore/ui/buttons/XUMenuButton.swift
+++ XUCore/ui/buttons/XUMenuButton.swift
@@ -8,16 +8,64 @@
 
 import Cocoa
 
+/// Alignment of the menu towards to the button.
+@objc public enum XUMenuAlignment: Int {
+	/// The menu is aligned with the right edge.
+	case rightEdge = 0
+	
+	/// The menu is aligned with the left edge.
+	case leftEdge = 1
+}
+
 /// This is a button that displays self.menu on click as well as on rightMouseDown
 /// - positioned correctly.
+@IBDesignable
 open class XUMenuButton: NSButton {
 
+	/// Menu alignment.
+	@IBInspectable public var menuAlignment: XUMenuAlignment = .rightEdge
+	
+	/// Font used for the menu.
+	@IBInspectable public var menuFont: NSFont? {
+		didSet {
+			self._updateMenuFont()
+		}
+	}
+	
+	/// Offset for displaying the menu. You can offset it to pixel-perfectly align
+	/// it with the text/image.
+	@IBInspectable public var menuOffset: CGSize = CGSize()
+	
 	@objc private func _displayMenu() {
 		guard let menu = self.menu else {
 			return
 		}
 		
-		menu.popUp(positioning: nil, at: CGPoint(x: 0.0, y: self.bounds.height), in: self)
+		var point = CGPoint(x: 0.0 + self.menuOffset.width, y: self.bounds.height + self.menuOffset.height)
+		switch self.menuAlignment {
+		case .leftEdge:
+			// It's fine like that
+			break
+		case .rightEdge:
+			let size = menu.size
+			point.x += self.bounds.width
+			point.x -= size.width
+		}
+		
+		menu.popUp(positioning: nil, at: point, in: self)
+	}
+	
+	private func _updateMenuFont() {
+		let font: NSFont
+		if let f = self.menuFont {
+			font = f
+		} else {
+			// Base this on control size.
+			let size = NSFont.systemFontSize(for: self.controlSize)
+			font = NSFont.systemFont(ofSize: size)
+		}
+		
+		self.menu?.font = font
 	}
 	
 	open override func awakeFromNib() {
@@ -25,6 +73,7 @@ open class XUMenuButton: NSButton {
 		self.action = #selector(XUMenuButton._displayMenu)
 		
 		self.menu?.localizeMenu()
+		self._updateMenuFont()
 		
 		super.awakeFromNib()
 	}
