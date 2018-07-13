fc7a634b835d8bf20ea4ee6ef8a56e183d2376d3
Charlie Monroe <charlie@charliemonroe.net>
2018-07-13 10:00:31 +0200

    New: XUStoryboardInstantiable

---
+++ XUCoreMobile/src/ui/util/XUStoryboardInstantiable.swift
@@ -0,0 +1,51 @@
+//
+//  XUStoryboardInstantiable.swift
+//  XUCoreMobile
+//
+//  Created by Charlie Monroe on 7/13/18.
+//  Copyright © 2018 Charlie Monroe Software. All rights reserved.
+//
+
+import UIKit
+
+/// Protocol for instantiating a controller from storyboard. Given the `Self`
+/// return this only works currently with final classes.
+public protocol XUStoryboardInstantiable {
+	
+	static func instantiateFromStoryboard() -> Self
+	
+}
+
+/// Special case of controllers within the main storyboard. As the storyboard
+/// is specified, all you need to supply is the identifier.
+public protocol XUMainStoryboardInstantiable: XUStoryboardInstantiable {
+	
+	/// Storyboard identifier.
+	static var storyboardIdentifier: String { get }
+	
+}
+
+public extension XUMainStoryboardInstantiable {
+	
+	public static func instantiateFromStoryboard() -> Self {
+		return self.instantiate(from: .main, identifier: self.storyboardIdentifier)
+	}
+	
+}
+
+public extension UIStoryboard {
+	
+	/// Main storyboard in the main bundle. Do not use this if you've renamed
+	/// the main storyboard or there is none in your project.
+	static let main: UIStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+	
+}
+
+public extension XUStoryboardInstantiable {
+	
+	/// Convenience method that casts the controller into Self.
+	public static func instantiate(from storyboard: UIStoryboard, identifier: String) -> Self {
+		return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+	}
+	
+}
